960c8debeb1f9f8ab17fd7a6aa4e4938fd93f33e
Michael Dadi <michaeldadi@users.noreply.github.com>
2026-02-01 19:23:30 -0500

    ci: Add Zig-based cross-compilation support across workflows, Makefile, and GoReleaser

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 	"time"
 
+	pgx "github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/riftdata/rift/internal/cow"
 	"github.com/riftdata/rift/internal/server"
